4f90754c795c7a58e55ccdd2124456b633405f7c
Sergiogb07 <sguebor0702@g.educaand.es>
2023-02-28 21:11:07 +0100

    Refactorización realizada

--- src/numerosprimos/CribaEratostenes.java
+++ src/numerosprimos/CribaEratostenes.java
@@ -1,83 +1,62 @@
 /**
  * Clase para generar todos los números primos de 1 hasta
- * un número máximo especificado por el usuario. Como 
+ * un número máximo especificado por el usuario. Como
  * algoritmo se utiliza la criba de Eratóstenes.
- * 
- * Eratóstenes de Cirene (276 a.C., Cirene, Libia – 194
- * a.C., Alejandría, Egipto) fue el primer hombre que 
- * calculó la circunferencia de la Tierra. También
- * se le conoce por su trabajo con calendarios que ya 
- * incluían años bisiestos y por dirigir la mítica 
- * biblioteca de Alejandría.
- * 
- * El algoritmo es bastante simple: Dado un vector de
- * enteros empezando en 2, se tachan todos los múltiplos
- * de 2. A continuación, se encuentra el siguiente
- * entero no tachado y se tachan todos sus múltiplos. El 
- * proceso se repite hasta que se pasa de la raíz cuadrada
- * del valor máximo. Todos los números que queden sin 
- * tachar son números primos.
- */ 
-
-package numerosprimos;    
-    
-public class CribaEratostenes 
-{ 
- /** 
- * Generar números primos de 1 a max
- * @param max es el valor máximo
- * @return Vector de números primos
  */
- public static int[] generarPrimos (int max) 
- { 
- int i,j; 
- if (max >= 2) { 
- 
- // Declaraciones
- int dim = max + 1; // Tamaño del array 
- boolean[] esPrimo = new boolean[dim]; 
- 
- // Inicializar el array
- for (i=0; i<dim; i++) 
- esPrimo[i] = true; 
- 
- // Eliminar el 0 y el 1, que no son primos
- esPrimo[0] = esPrimo[1] = false; 
- 
- // Criba
- for (i=2; i<Math.sqrt(dim)+1; i++) { 
- if (esPrimo[i]) {
- // Eliminar los múltiplos de i
- for (j=2*i; j<dim; j+=i) 
- esPrimo[j] = false; 
- } 
- }
- 
- // ¿Cuántos primos hay? 
- int cuenta = 0; 
- for (i=0; i<dim; i++) { 
- if (esPrimo[i]) 
- cuenta++; 
- } 
- // Rellenar el vector de números primos
- int[] primos = new int[cuenta]; 
- for (i=0, j=0; i<dim; i++) { 
- if (esPrimo[i])
- primos[j++] = i; 
- } 
- 
- return primos; 
- 
- } else { // max < 2 
- 
- return new int[0]; // Vector vacío
- } 
- }
+package numerosprimos;
+
+public class CribaEratostenes {
+
+    /**
+     * Generar números primos de 1 a max
+     *
+     * @param max es el valor máximo
+     * @return Vector de números primos
+     */
+    public static int[] generarPrimos(int max) {
+        if (max >= 2) {
+            int dimensionesArray = max + 1, contadorPrimos = 0;
+            boolean[] esPrimo = new boolean[dimensionesArray];
+            esPrimo = inicializarArray(dimensionesArray, esPrimo);
+            contadorPrimos = cuentaPrimos(contadorPrimos, dimensionesArray, esPrimo);
+            int[] primos = new int[contadorPrimos];
+            primos = guardarPrimosArray(primos, dimensionesArray, esPrimo);
+            return primos;
+        } else {
+            return new int[0];
+        }
+    }
+
+    public static boolean[] inicializarArray(int dimensionesArray, boolean[] esPrimo) {
+        for (int i = 2; i < dimensionesArray; i++) {
+            esPrimo[i] = true;
+        }
+        for (int i = 2; i < Math.sqrt(dimensionesArray) + 1; i++) { // Eliminar múltiplos
+            if (esPrimo[i]) {
+                for (int j = 2 * i; j < dimensionesArray; j += i) {
+                    esPrimo[j] = false;
+                }
+            }
+        }
+        return esPrimo;
+    }
+
+    public static int cuentaPrimos(int contadorPrimos, int dimensionesArray, boolean[] esPrimo) {
+        for (int i = 0; i < dimensionesArray; i++) {
+            if (esPrimo[i]) {
+                contadorPrimos++;
+            }
+        }
+        return contadorPrimos;
+    }
+
+    public static int[] guardarPrimosArray(int[] primos, int dimensionesArray, boolean[] esPrimo) {
+        for (int i = 0, j = 0; i < dimensionesArray; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+        return primos;
+    }
+
 }
-
-
-
-
-
-
-
